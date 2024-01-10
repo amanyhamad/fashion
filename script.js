4828b98a804e56703d6ab08b533cfa6d5a6f0eba
amanyhamad <152794656+amanyhamad@users.noreply.github.com>
2024-01-10 08:17:45 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+function itemAdded () {
+    alert('item added successfully');   
+}
+
+
+let email = document.getElementById("email").value;
+let phone = document.getElementById("phone").value;
+
+if (email.value == "") {
+function validate() {
+alert("please insert your email");
+}}
